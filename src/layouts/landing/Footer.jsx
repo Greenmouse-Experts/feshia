c54cdbf3b1c9ce6bc005baad3d473e722cac06e7
One Line Of Code at a Time 👨‍💻. <hmztadeleke@gmail.com>
2025-06-26 20:12:25 +0100

    Update

--- src/layouts/landing/Footer.jsx
+++ src/layouts/landing/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapPinIcon, PhoneIcon, EnvelopeIcon } from '@heroicons/react/24/outline';
+import { MapPinIcon, PhoneIcon, EnvelopeIcon, PaperAirplaneIcon } from '@heroicons/react/24/outline';
 import { Link } from 'react-router-dom';
 
 
@@ -8,102 +8,123 @@ const Footer = () => {
         <footer className="bg-black text-white only-top px-4">
             <div className="Resizer mx-auto">
                 {/* Desktop Layout */}
-                <div className="hidden md:grid md:grid-cols-5 gap-8">
-                    {/* CONTACT */}
-                    <div className="col-span-2">
-                        <h3 className="text-white text-lg font-bold mb-4">CONTACT</h3>
-                        <ul className="space-y-4 text-sm">
-                            <li>
-                                <strong>Nigeria</strong>
-                                <ul className="space-y-3 mt-3 ml-4 text-sx">
-                                    <li className="flex items-center mr-10">
-                                        <MapPinIcon className="h-4 w-4 text-[#ED268F] mr-2" />
-                                        7 Ikorodu Road, Maryland Behind Maryland BRT Bus Stop Maryland Lagos - Nigeria
-                                    </li>
-                                    <li className="flex items-center">
-                                        <PhoneIcon className="h-4 w-4 text-[#ED268F] mr-2" />
-                                        +234 901 994 7067
-                                    </li>
-                                    <li className="flex items-center">
-                                        <EnvelopeIcon className="h-4 w-4 text-[#ED268F] mr-2" />
-                                        nigeriaoffice@feshia.com
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <strong>Australia</strong>
-                                <ul className="space-y-3 mt-3 ml-4 text-sx">
-                                    <li className="flex items-center">
-                                        <MapPinIcon className="h-4 w-4 text-pink-600 mr-2" />
-                                        Box 221, Flinders Lane, VIC 8009
-                                    </li>
-                                    <li className="flex items-center">
-                                        <PhoneIcon className="h-4 w-4 text-pink-600 mr-2" />
-                                        +61 412 581 295
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
+                <div className="px-4">
+                    <div className="hidden md:grid md:grid-cols-5 gap-8 mb-8">
+                        {/* CONTACT */}
+                        <div className="col-span-2">
+                            <h3 className="text-white text-lg font-bold mb-4">CONTACT</h3>
+                            <ul className="space-y-4 text-sm">
+                                <li>
+                                    <strong>Nigeria</strong>
+                                    <ul className="space-y-3 mt-3 ml-4 text-base">
+                                        <li className="flex items-center mr-10">
+                                            <MapPinIcon className="h-4 w-4 text-[#ED268F] mr-2" />
+                                            7 Ikorodu Road, Maryland Behind Maryland BRT Bus Stop Maryland Lagos - Nigeria
+                                        </li>
+                                        <li className="flex items-center">
+                                            <PhoneIcon className="h-4 w-4 text-[#ED268F] mr-2" />
+                                            +234 901 994 7067
+                                        </li>
+                                        <li className="flex items-center">
+                                            <EnvelopeIcon className="h-4 w-4 text-[#ED268F] mr-2" />
+                                            nigeriaoffice@feshia.com
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <strong>Australia</strong>
+                                    <ul className="space-y-3 mt-3 ml-4 text-base">
+                                        <li className="flex items-center">
+                                            <MapPinIcon className="h-4 w-4 text-pink-600 mr-2" />
+                                            Box 221, Flinders Lane, VIC 8009
+                                        </li>
+                                        <li className="flex items-center">
+                                            <PhoneIcon className="h-4 w-4 text-pink-600 mr-2" />
+                                            +61 412 581 295
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+
+                        {/* COMPANY */}
+                        <div className="col-span-1">
+                            <h3 className="text-white text-lg font-bold mb-4">COMPANY</h3>
+                            <ul className="space-y-4 text-base">
+                                <li>About Us</li>
+                                <li>For Student</li>
+                                <li>For Institution</li>
+                                <li>Resources</li>
+                                <li>Events</li>
+                                <li>Apply Now</li>
+                            </ul>
+                        </div>
+
+                        {/* QUICK LINKS */}
+                        <div className="col-span-1">
+                            <h3 className="text-white text-lg font-bold mb-4">QUICK LINKS</h3>
+                            <ul className="space-y-4 text-base">
+                                <li>Articles</li>
+                                <li>Contact Us</li>
+                                <li>Disclaimer</li>
+                                <li>FAQs</li>
+                                <li>Privacy Policy</li>
+                                <li>Terms & Condition</li>
+                            </ul>
+                        </div>
+
+                        {/* STUDY ABROAD */}
+                        <div className="col-span-1">
+                            <h3 className="text-white text-lg font-bold mb-4">STUDY ABROAD</h3>
+                            <ul className="space-y-4 text-base">
+                                <li>Australia</li>
+                                <li>Canada</li>
+                                <li>United Kingdom</li>
+                                <li>United States</li>
+                                <li>Germany</li>
+                                <li>Netherlands</li>
+                                <li>France</li>
+                            </ul>
+                        </div>
                     </div>
 
-                    {/* COMPANY */}
-                    <div className="col-span-1">
-                        <h3 className="text-white text-lg font-bold mb-4">COMPANY</h3>
-                        <ul className="space-y-4 text-sm">
-                            <li>About Us</li>
-                            <li>For Student</li>
-                            <li>For Institution</li>
-                            <li>Resources</li>
-                            <li>Events</li>
-                            <li>Apply Now</li>
-                        </ul>
-                    </div>
 
-                    {/* QUICK LINKS */}
-                    <div className="col-span-1">
-                        <h3 className="text-white text-lg font-bold mb-4">QUICK LINKS</h3>
-                        <ul className="space-y-4 text-sm">
-                            <li>Articles</li>
-                            <li>Contact Us</li>
-                            <li>Disclaimer</li>
-                            <li>FAQs</li>
-                            <li>Privacy Policy</li>
-                            <li>Terms & Condition</li>
-                        </ul>
-                    </div>
+                    {/* Newsletter Section */}
+                    <div className="Resizer flex flex-col md:flex-row justify-between items-center w-full hidden md:block">
+                        <div className="w-full md:w-1/3 mb-6 md:mb-0">
+                            <h3 className="text-white text-base md:text-base font-bold mb-3">NEWSLETTER</h3>
+                            <form className="flex">
+                                <input
+                                    type="email"
+                                    placeholder="Enter your Email"
+                                    className="p-4 w-full text-xs bg-white rounded-l-sm italic text-black outline-none"
+                                />
+                                <button
+                                    type="submit"
+                                    className="bg-pink-600 hover:bg-pink-700 text-white px-4 flex items-center justify-center rounded-r-sm"
+                                >
+                                    <PaperAirplaneIcon className="w-5 h-5 transform rotate-45" />
+                                </button>
+                            </form>
+                        </div>
 
-                    {/* STUDY ABROAD */}
-                    <div className="col-span-1">
-                        <h3 className="text-white text-lg font-bold mb-4">STUDY ABROAD</h3>
-                        <ul className="space-y-4 text-sm">
-                            <li>Australia</li>
-                            <li>Canada</li>
-                            <li>United Kingdom</li>
-                            <li>United States</li>
-                            <li>Germany</li>
-                            <li>Netherlands</li>
-                            <li>France</li>
-                        </ul>
+                        <div className="w-full md:w-auto flex justify-center md:justify-end">
+                            <Link to="/">
+                                <img
+                                    src="https://res.cloudinary.com/greenmouse-tech/image/upload/v1750848536/feshia%20Images/Frame_1_puzgs4.png"
+                                    alt="Feshia"
+                                    className="h-16 hidden md:block"
+                                    draggable="false"
+                                />
+                            </Link>
+                        </div>
                     </div>
                 </div>
 
-
-                <div className="flex flex-col items-center justify-center mt-8 text-center">
-                    <Link to="/">
-                        <img
-                            src="https://res.cloudinary.com/greenmouse-tech/image/upload/v1750848536/feshia%20Images/Frame_1_puzgs4.png"
-                            alt="Feshia"
-                            className="h-16 hidden md:block"
-                            draggable="false"
-                        />
-                    </Link>
-
-                </div>
-
+                {/* Copyright Text */}
                 <div className="flex flex-col items-center justify-center mt-8 text-center">
                     <div className="hidden md:block">
-
-                        <p className="text-xs text-center mt-8">©️ 2025 Feshia, All Rights Reserved</p>
+                        <p className="text-base text-center mt-8">©️ {new Date().getFullYear()} Feshia, All Rights Reserved</p>
                     </div>
                 </div>
 
@@ -119,14 +140,14 @@ const Footer = () => {
                                     <strong>Nigeria</strong>
                                     <ul className="space-y-2 mt-2">
                                         <li>
-                                         7 Ikordodu Road, Maryland Behind
+                                            7 Ikordodu Road, Maryland Behind
                                             Maryland BRT Bus Stop Maryland Lagos - Nigeria
                                         </li>
                                         <li>
-                                       +234 901 994 7067
+                                            +234 901 994 7067
                                         </li>
                                         <li>
-                                        nigeriaoffice@feshia.com
+                                            nigeriaoffice@feshia.com
                                         </li>
                                     </ul>
                                 </li>
@@ -134,7 +155,7 @@ const Footer = () => {
                                     <strong>Australia</strong>
                                     <ul className="space-y-2 mt-2">
                                         <li>
-                                         Box 221, Flinders Lane, VIC 8009
+                                            Box 221, Flinders Lane, VIC 8009
                                         </li>
                                         <li>
                                             +61 412 581 295
@@ -183,6 +204,24 @@ const Footer = () => {
                                 <li>France</li>
                             </ul>
                         </div>
+
+                        <div className="w-full md:w-1/3 mb-6 md:mb-0">
+                            <h3 className="text-white text-base md:text-base font-bold mb-3">NEWSLETTER</h3>
+                            <form className="flex">
+                                <input
+                                    type="email"
+                                    placeholder="Enter your Email"
+                                    className="p-4 w-full text-xs bg-white rounded-l-sm italic text-black outline-none"
+                                />
+                                <button
+                                    type="submit"
+                                    className="bg-pink-600 hover:bg-pink-700 text-white px-4 flex items-center justify-center rounded-r-sm"
+                                >
+                                    <PaperAirplaneIcon className="w-5 h-5 transform rotate-45" />
+                                </button>
+                            </form>
+                        </div>
+
                         <div className="flex flex-col items-center justify-center mt-8 text-center">
                             <Link to="/">
                                 <img
@@ -192,7 +231,7 @@ const Footer = () => {
                                     draggable="false"
                                 />
                             </Link>
-                            <p className="text-xs mt-4">©️ 2025 Feshia, All Rights Reserved</p>
+                            <p className="text-xs mt-4">©️ {new Date().getFullYear()} Feshia, All Rights Reserved</p>
                         </div>
                     </div>
                 </div>
